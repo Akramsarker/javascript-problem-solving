85735b6e833d371a7d4feb2704262597d66f8536
Akramsarker <dev.mdakram@gmail.com>
2023-01-07 14:45:53 +0600

    Add solve new problem

--- index.js
+++ index.js
@@ -4,13 +4,25 @@
 
 // Find out leap Year
 
-function isLeapYear(year) {
-  const leapYear = year % 4;
-  if (leapYear === 0) {
-    console.log(year, "Is Leap Year");
-  } else {
-    console.log(year, "Is Not Leap Year");
-  }
-}
+// function isLeapYear(year) {
+//   const leapYear = year % 4;
+//   if (leapYear === 0) {
+//     console.log(year, "Is Leap Year");
+//   } else {
+//     console.log(year, "Is Not Leap Year");
+//   }
+// }
 
-isLeapYear(2020);
+// isLeapYear(2020);
+
+// Use to Array methods name split
+
+const sentences =
+  "Amar somar bangla ami tomai valovasi. chiro din tomar aksh, tomar vatach amar prane bajai bashi. amar soname bangla ami tomai valovashi.";
+
+// const split = sentences.split(' ')
+// const split = sentences.split(".");
+// const split = sentences.split(",");
+const split = sentences.split(" + ");
+
+console.log(split);
