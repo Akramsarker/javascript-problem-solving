51332b9102b4c1e6ab749da794f92a5e83769faf
Akramsarker <dev.mdakram@gmail.com>
2023-05-07 02:35:04 +0600

    Fix new problem in the page

--- index.js
+++ index.js
@@ -27,17 +27,30 @@
 
 // console.log(split);
 
-const arr = ["John", "Jane", "John", "Mike", "Jane"];
-function removeDuplicates(arr) {
-  let newArray = [];
-  for (let i = 0; i < arr.length; i++) {
-    const name = arr[i];
-    if (newArray.includes(name) === false) {
-      newArray.push(name);
-    }
-  }
-  return newArray;
-}
-console.log(removeDuplicates(arr));
+// const arr = ["John", "Jane", "John", "Mike", "Jane"];
+// function removeDuplicates(arr) {
+//   let newArray = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     const name = arr[i];
+//     if (newArray.includes(name) === false) {
+//       newArray.push(name);
+//     }
+//   }
+//   return newArray;
+// }
+// console.log(removeDuplicates(arr));
 // const uniqueArr = [...new Set(arr)];
 // console.log(uniqueArr); // Output: ["John", "Jane", "Mike"]
+
+// Foo Bar
+for (let i = 0; i <= 50; i++) {
+  if (i % 3 === 0) {
+    console.log("foo");
+  } else if (i % 5 === 0) {
+    console.log("bar");
+  } else if (i % 3 === 0 && i % 5 === 0) {
+    console.log("Foo Bar");
+  } else {
+    console.log(i);
+  }
+}
